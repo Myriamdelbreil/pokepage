6c535efe418fa3550ead611f5381bae85a50831c
Myriam Delbreil <myriam.delbreil@live.fr>
2024-04-17 22:51:20 +0200

    api-consumer: create service to consume external API data (only get requests)

---
+++ spec/services/api_consumer_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+::RSpec.describe ::ApiConsumer, type: :service do
+  subject do
+    described_class.new(url).call
+  end
+
+  let(:url) { "https://pokeapi.co/api/v2/pokemon/90"}
+
+  context "when unvalid url" do
+    context "when not an url" do
+      let(:url) { "fhff" }
+
+      it "raises an error" do
+        expect { subject }.to raise_error(::Errno::ECONNREFUSED)
+      end
+    end
+
+    context "when an unexisting url" do
+    end
+  end
+
+  context "when valid url" do
+    it "returns a 200" do
+      expect(subject.response.code).to eq("200")
+    end
+
+    it "returns a json with expected attributes" do
+      expect(subject.keys).to include("id", "abilities", "types", "past_types", "weight")
+    end
+  end
+end
