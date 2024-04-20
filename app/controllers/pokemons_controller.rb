d56b29a7d05d14917e05957e6e8f3c9082795048
Myriam Delbreil <myriam.delbreil@live.fr>
2024-04-20 17:24:55 +0200

    index: add pagination

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,11 +2,15 @@
 
 class PokemonsController < ApplicationController
   def index
-    results = ::ApiConsumer.new("/pokemon").call["results"]
+    @offset = params[:offset].nil? ? '0' : params[:offset]
+    response = ::ApiConsumer.new("/pokemon?offset=#{@offset}&limit=20").call
+    results = response['results']
 
-    @pokemons = results.pluck("name").map do |name|
+    @pokemons = results.pluck('name').map do |name|
       ::Pokemon.new(name)
     end
+    @next_page = response['next']
+    @previous_page = response['previous']
   end
 
   def show
